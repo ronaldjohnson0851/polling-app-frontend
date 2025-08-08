7dddba1f196499cecebd65de2ee4d5048343b2a1
RgJ3rd <137233800+RgJ3rd@users.noreply.github.com>
2025-08-08 14:46:19 -0400

    Working on this

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';
+import PollList from './components/poll/PollList';
+
+const { Content } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Layout className="app-container">
+          <Content className="app-content">
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<PollList />} />
+              </Routes>
+            </div>
+          </Content>
+        </Layout>
+      </Router>
+    );
+  }
+}
+
+export default App;
